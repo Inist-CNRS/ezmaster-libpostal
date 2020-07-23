e3bd16dabfc62471b6b2c4ea144d6932fdd1a13d
Nicolas Thouvenin <nthouvenin@gmail.com>
2020-07-23 15:00:28 +0200

    add new entrypoint : parse

--- local/src/parse-address.js
+++ local/src/parse-address.js
@@ -1,15 +1,21 @@
 import postal from '@cymen/node-postal';
 
+const parse = (input) => ({
+    id: input,
+    value: postal.parser
+        .parse_address(String(input).trim())
+        .reduce((obj, cur) => ({ ...obj, [cur.component]: cur.value }), {}),
+});
+
 export default function parseAddress(data, feed) {
     if (this.isLast()) {
         return feed.close();
     }
-    if (typeof data !== 'string') {
-        return feed.send(data);
+    if (Array.isArray(data)) {
+        return feed.send(data.map(parse));
     }
-    const addresses = postal.parser.parse_address(data.trim());
-    return feed.send({
-        id: data,
-        value: addresses,
-    });
+    if (typeof data === 'string') {
+        return feed.send(parse(data));
+    }
+    return feed.send(data);
 }
