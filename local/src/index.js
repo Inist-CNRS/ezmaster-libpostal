7023e5a9c4eea3b8eb04df6330c34fbb08c62fef
Nicolas Thouvenin <nthouvenin@gmail.com>
2020-07-24 17:05:12 +0200

    bufferize was a bad idea

--- local/src/index.js
+++ local/src/index.js
@@ -1,7 +1,9 @@
 import parseAddress from './parse-address';
+import parseAddressWith from './parse-address-with';
 import expandAddress from './expand-address';
 
 export default {
     parseAddress,
+    parseAddressWith,
     expandAddress,
 };
